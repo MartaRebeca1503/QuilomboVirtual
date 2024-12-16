eb96b79d1d6bc0f00b8d0d8aad2895a7d8eaf23d
Kelvis Duank <KelvisDuankxkb@gmail.com>
2024-12-16 15:41:27 -0300

    Final Adjusts

---
+++ src/js/about.js
@@ -0,0 +1,21 @@
+// Função para abrir o modal específico
+function openModal(developerId) {
+    const modal = document.getElementById(`modal${developerId}`);
+    modal.style.display = "block";
+}
+
+// Função para fechar o modal específico
+function closeModal(developerId) {
+    const modal = document.getElementById(`modal${developerId}`);
+    modal.style.display = "none";
+}
+
+// Fechar modal se clicar fora da caixa modal
+window.onclick = function(event) {
+    if (event.target.classList.contains('modal')) {
+        closeModal(1);
+        closeModal(2);
+        closeModal(3);
+        closeModal(4);
+    }
+};
--- src/js/culture.js
+++ src/js/culture.js
@@ -6,7 +6,7 @@ const menuHTML = `
             <a href="../index.html"><li class="nav">Home</li></a>
            <a href="contato/"> <li class="nav">Contato</li></a>
           <a href="https://www.vakinha.com.br/buscar-vaquinha?page=1&term=Vaquinhas%20para%20pessoas%20negras">  <li class="nav">Apoio</li></a>
-            <a href="sobre/sobre.html"><li class="nav">Sobre</li></a>
+            <a href="about.html"><li class="nav">Sobre</li></a>
         </ol>
     </ul>
 </nav>
